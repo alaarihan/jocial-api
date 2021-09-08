beedf1185cb5b9055b05861c392dc4f780580399
Alaa Rihan <alaa.addeen@gmail.com>
2021-09-08 20:43:53 +0300

    Improvement

--- src/server.ts
+++ src/server.ts
@@ -30,7 +30,7 @@ async function start() {
   app.register(mercurius, {
     schema: schemaWithMiddlewares,
     context: createContext,
-    graphiql: true,
+    graphiql: process.env.INTROSPECTIONS==='on',
     subscription: {
       context: (_, req) => {
         return createContext(req)
